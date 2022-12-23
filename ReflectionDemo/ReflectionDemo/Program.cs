69aa075b8a3b2bac1d3000bb3ccc7014478ee32b
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-23 10:28:51 +0530

    Program of RefectionOnCustomer

---
+++ ReflectionDemo/ReflectionDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using ReflectionDemoProject;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflection Demo");
+
+            ReflectionOnCustomer.ReflectionTest();
+            Console.ReadKey();
+        }
+    }
+}
